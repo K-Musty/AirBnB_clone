e87a6075e9d70173b9c7fbfecb9bf6ad0ce2b376
K-Musty <kmustapha9564@gmail.com>
2024-03-09 14:35:15 -0800

    __init__.py file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,7 @@
 import os.path
 import json
 from models.base_model import BaseModel
+import datetime
 
 class FileStorage:
     """Type class File Storage"""
@@ -15,28 +16,80 @@ class FileStorage:
         return FileStorage.__objects
 
     def new(self, obj):
-        """Type method new"""
-        FSobjdict = FileStorage.__objects
-        object_name = obj.__class__.__name__
-        FSobjdict["{}.{}".format(object_name, obj.id)] = obj
+        """Sets new obj in __objects dictionary."""
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        FileStorage.__objects[key] = obj
 
     def save(self):
-        """Type method save"""
-        FSobjdict = FileStorage.__objects
-        obj_dict = {obj: FSobjdict[obj].to_dict() for obj in FSobjdict.keys()}
-        with open(FileStorage.__file_path, "w") as f:
-            json.dump(obj_dict, f)
+        """Serialzes __objects to JSON file."""
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
+            d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
+            json.dump(d, f)
+
+    def classes(self):
+        """Return dictionary of valid classes and references."""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        classes = {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
+        return classes
 
     def reload(self):
-        """Type method reaload"""
-        if os.path.exists(FileStorage.__file_path):
-            with open(FileStorage.__file_path) as f:
-                obj_dict = json.load(f)
-                for obj in obj_dict.values():
-                    cls_d = obj["__class__"]
-                    del obj["__class__"]
-                    self.new(eval(cls_d)(**obj))
+        """Function Deserializes JSON file into __objects."""
+        if not os.path.isfile(FileStorage.__file_path):
             return
+        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+            obj_dict = json.load(f)
+            obj_dict = {k: self.classes()[v["__class__"]](**v)
+                        for k, v in obj_dict.items()}
+            FileStorage.__objects = obj_dict
 
-
+    def attributes(self):
+        """Returns the valid attributes and their types for classname."""
+        attributes = {
+            "BaseModel":
+                     {"id": str,
+                      "created_at": datetime.datetime,
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first_name": str,
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+            {"place_id": str,
+                         "user_id": str,
+                         "text": str}
+        }
+        return attributes
 
