902e4f80ce4c0fecf7109174d5bc969bdda2bdc0
K-Musty <kmustapha9564@gmail.com>
2024-03-10 09:24:30 -0700

    Console and more models with tests included

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+"""Class that will
+represents
+the amenity"""
+
+
+class Amenity(BaseModel):
+    """Initializing amenity basemodel class"""
+    name = ''
---
+++ models/city.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+"""Class for city"""
+
+
+class City(BaseModel):
+
+    """City class basemodel instance"""
+    state_id = ""
+    name = ""
+
